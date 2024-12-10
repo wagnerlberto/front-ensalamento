63ed83c82dd386305bad4bb64d9561ba7fe0863f
Wagner L. Berto <wagnerlberto@hotmail.com>
2024-12-10 19:51:00 -0300

    lista de salas set up

--- script.js
+++ script.js
@@ -6,45 +6,9 @@ window.onload = function() {
   const btnMatutino = document.querySelector('#btn-matutino');
   const sectionMensagemHome = document.querySelector('#section-mensagem-home');
   const sectionListaSalas = document.querySelector('#section-lista-salas');
-
-  let salas = [
-    {
-      disciplina: "CMF Sistema Digestório, Endócrino e Renal (FISIOTERAPIA, ENFERMAGEM)",
-      diaDaSemana: "6ªf",
-      sala: "C6",
-      professor: "JOÃO"
-    },
-    {
-      disciplina: "Cien. Morfofuncionais do Aparelho Locomotor - Membros Infer. e Coluna Vertebral (FISIOTERAPIA)",
-      diaDaSemana: "3ªf",
-      sala: "B4",
-      professor: "MAGDA"
-    },
-    {
-      disciplina: "Cien. Morfofuncionais do Aparelho Locomotor - Membros Infer. e Coluna Vertebral (FISIOTERAPIA)",
-      diaDaSemana: "3ªf",
-      sala: "B4",
-      professor: "MAGDA"
-    },
-    {
-      disciplina: "Cien. Morfofuncionais do Aparelho Locomotor - Membros Infer. e Coluna Vertebral (FISIOTERAPIA)",
-      diaDaSemana: "3ªf",
-      sala: "B4",
-      professor: "MAGDA"
-    },
-    {
-      disciplina: "Cien. Morfofuncionais do Aparelho Locomotor - Membros Infer. e Coluna Vertebral (FISIOTERAPIA)",
-      diaDaSemana: "3ªf",
-      sala: "B4",
-      professor: "MAGDA"
-    },
-    {
-      disciplina: "Cien. Morfofuncionais do Aparelho Locomotor - Membros Infer. e Coluna Vertebral (FISIOTERAPIA)",
-      diaDaSemana: "3ªf",
-      sala: "B4",
-      professor: "MAGDA"
-    }
-  ];
+  // Usar o URL abaixo em produção
+  // const urlBack = "https://back-ensalamento.onrender.com";
+  const urlBack = "http://localhost:3000";
 
   function atualizarPrincipal() {
     // Mostrar mensagem de boas vindas
@@ -71,35 +35,92 @@ window.onload = function() {
     carregandoMessage.style.display = "none";
   });
 
-  btnMatutino.addEventListener('click', () => {
-    atualizarMatutino();
-    // Atualizar lista de salas
-    atualizarLista(salas);
-    carregandoMessage.style.display = "none";
-  });
-  
+  async function pegarTodasAsSalas() {
+    const endPoint = `${urlBack}/ensalamentoM`;
+    const res = await fetch(`${endPoint}`);
+    // console.log(res);
+
+    const data = await res.json();
+    // console.log(data);
+
+    return data;
+  }
+
   const criarSala = (sala) => {
     return `
       <article class='articleLinhaTabela'>
         <div class='disciplinaLinhaTabela'>${sala.disciplina}</div>
-        <span class='diaDaSemanaLinhaTabela'>${sala.diaDaSemana}</span>
+        <span class='dia-da-semana__LinhaTabela'>${sala.dia_da_semana}</span>
         <span class='professorLinhaTabela'>${sala.professor}</span>
         <div class='salaLinhaTabela'>${sala.sala}</div>
       </article> 
     `;
   }
 
-  const atualizarLista = (salas) => {
+  async function atualizarLista () {
+    // let salas = [
+    //   {
+    //     disciplina: "CMF Sistema Digestório, Endócrino e Renal (FISIOTERAPIA, ENFERMAGEM)",
+    //     diaDaSemana: "6ªf",
+    //     sala: "C6",
+    //     professor: "JOÃO"
+    //   },
+    //   {
+    //     disciplina: "Cien. Morfofuncionais do Aparelho Locomotor - Membros Infer. e Coluna Vertebral (FISIOTERAPIA)",
+    //     diaDaSemana: "3ªf",
+    //     sala: "B4",
+    //     professor: "MAGDA"
+    //   },
+    //   {
+    //     disciplina: "Cien. Morfofuncionais do Aparelho Locomotor - Membros Infer. e Coluna Vertebral (FISIOTERAPIA)",
+    //     diaDaSemana: "3ªf",
+    //     sala: "B4",
+    //     professor: "MAGDA"
+    //   },
+    //   {
+    //     disciplina: "Cien. Morfofuncionais do Aparelho Locomotor - Membros Infer. e Coluna Vertebral (FISIOTERAPIA)",
+    //     diaDaSemana: "3ªf",
+    //     sala: "B4",
+    //     professor: "MAGDA"
+    //   },
+    //   {
+    //     disciplina: "Cien. Morfofuncionais do Aparelho Locomotor - Membros Infer. e Coluna Vertebral (FISIOTERAPIA)",
+    //     diaDaSemana: "3ªf",
+    //     sala: "B4",
+    //     professor: "MAGDA"
+    //   },
+    //   {
+    //     disciplina: "Cien. Morfofuncionais do Aparelho Locomotor - Membros Infer. e Coluna Vertebral (FISIOTERAPIA)",
+    //     diaDaSemana: "3ªf",
+    //     sala: "B4",
+    //     professor: "MAGDA"
+    //   }
+    // ];
+
+    const endPoint = `${urlBack}/ensalamentoM`;
+    const res = await fetch(`${endPoint}`);
+    // console.log(res);
+
+    const data = await res.json();
+    // console.log(data);
+
     let output = "";
 
-    for(let sala of salas)
+    for(let sala of data)
       output += criarSala(sala);
 
     document
-    .querySelector('.articleLinhaTabela')
-    .innerHTML += output;
+    .querySelector('#section-lista-salas')
+    .innerHTML = output;
   }
 
+  btnMatutino.addEventListener('click', () => {
+    atualizarMatutino();
+    // Atualizar lista de salas
+    atualizarLista();
+    carregandoMessage.style.display = "none";
+  });
+  
   atualizarPrincipal();
   // atualizarMatutino();
 
