51db39a7a45238064f211ed5fb8d594a7d38d6c9
Wagner L. Berto <wagnerlberto@hotmail.com>
2024-12-11 12:45:37 -0300

    procurar functionality set up

--- script.js
+++ script.js
@@ -1,50 +1,51 @@
 window.onload = function() { 
-  console.log('>>>PASSOU POR AQUI<<<');
+  // console.log('>>>PASSOU POR AQUI<<<');
   
-  const btnPrincipal = document.querySelector('#btn-principal');
-  const carregandoMessage = document.querySelector('#carregando-message');
-  const btnMatutino = document.querySelector('#btn-matutino');
-  const sectionMensagemHome = document.querySelector('#section-mensagem-home');
-  const sectionListaSalas = document.querySelector('#section-lista-salas');
   // Usar o URL abaixo em produção
-  const urlBack = "https://back-ensalamento.onrender.com";
-  // const urlBack = "http://localhost:3000";
+  // const urlBase = "https://back-ensalamento.onrender.com";
+  const urlBase = "http://localhost:3000";
+
+  const btnPrincipal = document.querySelector('#btn-principal');
+  const sectionMensagemHome = document.querySelector('#section-mensagem-home');
+  const btnMatutino = document.querySelector('#btn-matutino');
+  const sectionProcura = document.querySelector('#section-procura');
+  const edtProcura = document.querySelector('#edt-procura');
+  const carregandoMessage = document.querySelector('#carregando-message');
+  const btnProcurar = document.querySelector('#btn-procurar');
+  const sectionListaSalas = document.querySelector('#section-lista-salas');
 
   function atualizarPrincipal() {
     // Mostrar mensagem de boas vindas
     sectionMensagemHome.style.display = "";
+    // Esconder mensagem de carregando
     carregandoMessage.style.display = "none";
+    // Esconder barra de pesquisa
+    sectionProcura.style.display = "none";
     // Esconder lista de salas
     sectionListaSalas.style.display = "none";
+    // Colorir botão ativo
     btnPrincipal.style.backgroundColor = "Khaki";
+    // Descolorir botões inativos
     btnMatutino.style.backgroundColor = "orange";
   }
 
   function atualizarMatutino() {
     // Esconder mensagem de boas vindas
     sectionMensagemHome.style.display = "none";
-    carregandoMessage.style.display = "";
+    // Mostrar barra de pesquisa
+    sectionProcura.style.display = "";
     // Mostrar lista de salas
     sectionListaSalas.style.display = "";
-    btnPrincipal.style.backgroundColor = "orange";
+    // Colorir botão ativo
     btnMatutino.style.backgroundColor = "Khaki";
+    // Descolorir botões inativos
+    btnPrincipal.style.backgroundColor = "orange";
   }
 
   btnPrincipal.addEventListener('click', () => {
+    // Montar página principal
     atualizarPrincipal();
-    carregandoMessage.style.display = "none";
   });
-
-  async function pegarTodasAsSalas() {
-    const endPoint = `${urlBack}/ensalamentoM`;
-    const res = await fetch(`${endPoint}`);
-    // console.log(res);
-
-    const data = await res.json();
-    // console.log(data);
-
-    return data;
-  }
 
   const criarSala = (sala) => {
     return `
@@ -58,70 +59,39 @@ window.onload = function() {
   }
 
   async function atualizarLista () {
-    // let salas = [
-    //   {
-    //     disciplina: "CMF Sistema Digestório, Endócrino e Renal (FISIOTERAPIA, ENFERMAGEM)",
-    //     diaDaSemana: "6ªf",
-    //     sala: "C6",
-    //     professor: "JOÃO"
-    //   },
-    //   {
-    //     disciplina: "Cien. Morfofuncionais do Aparelho Locomotor - Membros Infer. e Coluna Vertebral (FISIOTERAPIA)",
-    //     diaDaSemana: "3ªf",
-    //     sala: "B4",
-    //     professor: "MAGDA"
-    //   },
-    //   {
-    //     disciplina: "Cien. Morfofuncionais do Aparelho Locomotor - Membros Infer. e Coluna Vertebral (FISIOTERAPIA)",
-    //     diaDaSemana: "3ªf",
-    //     sala: "B4",
-    //     professor: "MAGDA"
-    //   },
-    //   {
-    //     disciplina: "Cien. Morfofuncionais do Aparelho Locomotor - Membros Infer. e Coluna Vertebral (FISIOTERAPIA)",
-    //     diaDaSemana: "3ªf",
-    //     sala: "B4",
-    //     professor: "MAGDA"
-    //   },
-    //   {
-    //     disciplina: "Cien. Morfofuncionais do Aparelho Locomotor - Membros Infer. e Coluna Vertebral (FISIOTERAPIA)",
-    //     diaDaSemana: "3ªf",
-    //     sala: "B4",
-    //     professor: "MAGDA"
-    //   },
-    //   {
-    //     disciplina: "Cien. Morfofuncionais do Aparelho Locomotor - Membros Infer. e Coluna Vertebral (FISIOTERAPIA)",
-    //     diaDaSemana: "3ªf",
-    //     sala: "B4",
-    //     professor: "MAGDA"
-    //   }
-    // ];
+    // Mostrar mensagem de carregando
+    carregandoMessage.style.display = "";
 
-    const endPoint = `${urlBack}/ensalamentoM`;
+    const param = `/${edtProcura.value}`;
+    const path = `/ensalamentoM`;
+    const endPoint = `${urlBase}${path}${param}`;
     const res = await fetch(`${endPoint}`);
-    // console.log(res);
-
     const data = await res.json();
-    // console.log(data);
 
     let output = "";
-
     for(let sala of data)
       output += criarSala(sala);
 
     document
     .querySelector('#section-lista-salas')
     .innerHTML = output;
+
+    // Esconder mensagem de carregando
+    carregandoMessage.style.display = "none";
   }
 
   btnMatutino.addEventListener('click', () => {
+    // Montar página matutino
     atualizarMatutino();
-    // Atualizar lista de salas
-    atualizarLista();
-    carregandoMessage.style.display = "none";
   });
   
+  btnProcurar.addEventListener('click', () => {
+    // Atualizar lista de salas
+    atualizarLista();
+  });
+
+  // Iniciar
+  // Montar página principal
   atualizarPrincipal();
-  // atualizarMatutino();
 
 }
